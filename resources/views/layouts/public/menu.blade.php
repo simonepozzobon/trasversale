e42b1fbdece68cd0cdd7d9dadd4b7afd9dd110f7
Angus Simons <info@simonepozzobon.com>
2019-03-04 22:49:53 +0100

    Logo e disposizione dei menu

--- resources/views/layouts/public/menu.blade.php
+++ resources/views/layouts/public/menu.blade.php
@@ -1,53 +1,73 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-white">
-    <a class="navbar-brand" href="#">Logo</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">F</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">T</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Ln</a>
-            </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        </form>
+<nav class="navbar navbar-main navbar-expand-lg navbar-light bg-white">
+    <div class="navbar-main__head">
+        <a class="navbar-brand" href="#">
+            <img src="/svg/logo.svg" alt="" style="width: 250px">
+        </a>
     </div>
-</nav>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <span class="navbar-brand">economia della conoscenza</span>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="#">chi siamo</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">formazione</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">educational</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">corsi</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">notizie</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">contatti</a>
-            </li>
-        </ul>
+    <div class="navbar-collapse navbar-main__container" id="navbarSupportedContent">
+        <div class="navbar-main__topbar">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">F</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">T</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Ln</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            </form>
+        </div>
+        <div class="navbar-main__menubar">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">chi siamo</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">formazione</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">educational</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">corsi</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">notizie</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">contatti</a>
+                </li>
+            </ul>
+        </div>
+        <div class="navbar-main__submenu">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">il nostro approcio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">il team</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">la rete</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">la nostra storia</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">certificazioni</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">cv della società</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">archivio progetti realizzati</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
