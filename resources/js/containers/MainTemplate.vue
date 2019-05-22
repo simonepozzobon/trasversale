6e13aabf138a3152d99b3c5a18db292dc570c579
Angus Simons <info@simonepozzobon.com>
2019-05-22 14:08:19 +0200

    Footer

--- resources/js/containers/MainTemplate.vue
+++ resources/js/containers/MainTemplate.vue
@@ -1,43 +1,48 @@
 <template lang="html">
-    <div class="container">
-        <main-menu :pages="parsedPages"/>
-        <div>
-            <div class="row custom-blue main-content">
-                <div class="col-md-9 custom-green main-content__content">
-                    <router-view />
-                </div>
-                <div class="col-md-3 custom-red main-content__sidebar" ref="sidebar">
-                    <ui-sidebar-image
-                        src="/dummies/il-team/img-1.png"
-                        alt="titolo"/>
-                    <ui-sidebar-paragraph
-                        content="Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere, whose upper half was strewn with silver."/>
-                    <ui-calendar
-                        />
-                    <ui-sidebar-title
-                        title="Partner Istituzionali"/>
-                    <ui-partner
-                        v-for="partner in partners"
-                        :key="partner.id"
-                        :title="partner.title"
-                        :img="partner.img"
-                        :size="60"
-                        />
+    <div class="main">
+        <div class="container">
+            <main-menu :pages="parsedPages"/>
+            <div>
+                <div class="row custom-blue main-content">
+                    <div class="col-md-9 custom-green main-content__content">
+                        <router-view />
+                    </div>
+                    <div class="col-md-3 custom-red main-content__sidebar" ref="sidebar">
+                        <ui-sidebar-image
+                            src="/dummies/il-team/img-1.png"
+                            alt="titolo"/>
+                        <ui-sidebar-paragraph
+                            content="Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere, whose upper half was strewn with silver."/>
+                        <ui-calendar
+                            />
+                        <ui-sidebar-title
+                            title="Partner Istituzionali"/>
+                        <ui-partner
+                            v-for="partner in partners"
+                            :key="partner.id"
+                            :title="partner.title"
+                            :img="partner.img"
+                            :size="60"
+                            />
+                    </div>
                 </div>
             </div>
         </div>
+        <main-footer />
     </div>
 </template>
 
 <script>
 import Partners from '../contents/Partners'
 import MainMenu from './MainMenu.vue'
+import MainFooter from './MainFooter.vue'
 import { UiCalendar, UiPartner, UiSidebarImage, UiSidebarParagraph, UiSidebarTitle, UiTitle, } from '../ui'
 
 export default {
     name: 'MainTemplate',
     components: {
         MainMenu,
+        MainFooter,
         UiCalendar,
         UiPartner,
         UiSidebarImage,
@@ -78,6 +83,16 @@ export default {
 <style lang="scss" scoped>
 @import '~styles/shared';
 $debug-opacity: 0 !default;
+
+.main {
+    min-height: 100vh;
+    max-width: 100vw;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
 
 .main-content {
     &__content {
