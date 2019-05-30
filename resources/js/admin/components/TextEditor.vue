1e18ce4bed74e00ff4a71415fd87b51ef7612a92
Angus Simons <info@simonepozzobon.com>
2019-05-30 19:40:49 +0200

    Aggiunge Link sull'editor

--- resources/js/admin/components/TextEditor.vue
+++ resources/js/admin/components/TextEditor.vue
@@ -1,4 +1,4 @@
-<template lang="html">
+<template>
     <div class="editor">
         <editor-menu-bar
             :editor="editor"
@@ -42,6 +42,37 @@
 
             </div>
         </editor-menu-bar>
+        <editor-menu-bubble
+            class="menububble"
+            :editor="editor"
+            @hide="hideLinkMenu"
+            v-slot="{ commands, isActive, getMarkAttrs, menu }"
+        >
+            <div
+                class="menububble"
+                :class="{ 'is-active': menu.isActive }"
+                :style="`left: ${menu.left}px; bottom: ${menu.bottom}px;`"
+            >
+
+            <form class="menububble__form" v-if="linkMenuIsActive" @submit.prevent="setLinkUrl(commands.link, linkUrl)">
+                <input class="menububble__input" type="text" v-model="linkUrl" placeholder="https://" ref="linkInput" @keydown.esc="hideLinkMenu"/>
+                <button class="menububble__button" @click="setLinkUrl(commands.link, null)" type="button">
+                    Elimina
+                </button>
+            </form>
+
+            <template v-else>
+                <button
+                    class="menububble__button"
+                    @click="showLinkMenu(getMarkAttrs('link'))"
+                    :class="{ 'is-active': isActive.link() }"
+                >
+                    <span>{{ isActive.link() ? 'Modifica Link' : 'Aggiungi Link'}}</span>
+                </button>
+            </template>
+
+            </div>
+        </editor-menu-bubble>
         <editor-content
             class="editor__content"
             :editor="editor">
@@ -57,6 +88,7 @@ import {
     Editor,
     EditorContent,
     EditorMenuBar,
+    EditorMenuBubble,
 } from 'tiptap'
 
 
@@ -85,6 +117,7 @@ export default {
     components: {
         EditorContent,
         EditorMenuBar,
+        EditorMenuBubble,
     },
     props: {
         initial: {
@@ -97,6 +130,8 @@ export default {
             editor: null,
             html: null,
             json: null,
+            linkUrl: null,
+            linkMenuIsActive: false,
         }
     },
     methods: {
@@ -136,6 +171,22 @@ export default {
                 this.$emit('update', this.json, this.html)
                 // console.log('updated');
             })
+        },
+        showLinkMenu: function(attrs) {
+            this.linkUrl = attrs.href
+            this.linkMenuIsActive = true
+            this.$nextTick(() => {
+                this.$refs.linkInput.focus()
+            })
+        },
+        hideLinkMenu: function() {
+            this.linkUrl = null
+            this.linkMenuIsActive = false
+        },
+        setLinkUrl: function(command, url) {
+            command({ href: url })
+            this.hideLinkMenu()
+            this.editor.focus()
         },
     },
     mounted: function() {
