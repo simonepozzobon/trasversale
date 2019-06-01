d261a731866b549f6140b74a3b7ce99975bc1fa4
Angus Simons <info@simonepozzobon.com>
2019-06-01 04:25:02 +0200

    Ultime modifiche

--- resources/js/admin/containers/NewPageTemplate.vue
+++ resources/js/admin/containers/NewPageTemplate.vue
@@ -14,21 +14,37 @@
                                     @click="addRow">
                                     Aggiungi Riga
                                 </button> -->
-                            <button class="btn btn-outline-primary" @click="addComponent">
+                            <button
+                                class="btn btn-outline-primary"
+                                @click="addComponent"
+                            >
                                 Aggiungi Componente
                             </button>
                         </div>
                     </div>
                 </div>
                 <div class="page-template__content">
-                    <module-container v-for="(module, i) in cached" :key="i" :item="module" :model="model" :model-idx="modelIdx" @deleted="deleteComponent" />
+                    <module-container
+                        v-for="(module, i) in cached"
+                        :key="i"
+                        :item="module"
+                        :model="model"
+                        :model-idx="modelIdx"
+                        @deleted="deleteComponent"
+                    />
                 </div>
                 <div class="page-template__footer">
-                    <button class="btn btn-outline-primary" @click="addComponent">
+                    <button
+                        class="btn btn-outline-primary"
+                        @click="addComponent"
+                    >
                         Aggiungi Componente
                     </button>
                 </div>
-                <components-list ref="componentSelector" @new-component="newComponent" />
+                <components-list
+                    ref="componentSelector"
+                    @new-component="newComponent"
+                />
             </div>
             <div class="page-template__main col-12">
                 <div class="page-template__header">
@@ -37,21 +53,37 @@
                             <h1 class="pt-3">Sidebar</h1>
                         </div>
                         <div class="page-template__action">
-                            <button class="btn btn-outline-primary" @click="addComponentSide">
+                            <button
+                                class="btn btn-outline-primary"
+                                @click="addComponentSide"
+                            >
                                 Aggiungi Componente
                             </button>
                         </div>
                     </div>
                 </div>
                 <div class="page-template__content">
-                    <module-container v-for="(module, i) in cachedSides" :key="i" :item="module" :model="model" :model-idx="modelIdx" @deleted="deleteComponentSide" />
+                    <module-container
+                        v-for="(module, i) in cachedSides"
+                        :key="i"
+                        :item="module"
+                        :model="model"
+                        :model-idx="modelIdx"
+                        @deleted="deleteComponentSide"
+                    />
                 </div>
                 <div class="page-template__footer">
-                    <button class="btn btn-outline-primary" @click="addComponentSide">
+                    <button
+                        class="btn btn-outline-primary"
+                        @click="addComponentSide"
+                    >
                         Aggiungi Componente
                     </button>
                 </div>
-                <components-list ref="sidebarSelector" @new-component="newComponentSide" />
+                <components-list
+                    ref="sidebarSelector"
+                    @new-component="newComponentSide"
+                />
             </div>
         </div>
     </div>
@@ -91,13 +123,13 @@ export default {
         },
         modules: {
             type: Array,
-            default: function () {
+            default: function() {
                 return []
             },
         },
         sides: {
             type: Array,
-            default: function () {
+            default: function() {
                 return []
             },
         },
@@ -106,7 +138,7 @@ export default {
             default: false,
         },
     },
-    data: function () {
+    data: function() {
         return {
             component: null,
             moduleType: null,
@@ -118,29 +150,29 @@ export default {
         }
     },
     computed: {
-        contentClass: function () {
+        contentClass: function() {
             if (this.hasSidebar) {
                 return 'col-md-9'
             }
         },
     },
     watch: {
-        '$route.params': function (params) {
+        '$route.params': function(params) {
             console.log('params cambiato', params);
         },
-        modules: function (modules) {
+        modules: function(modules) {
             this.init()
         }
     },
     methods: {
-        init: function () {
+        init: function() {
             // imposta una variabile intermedia per poter modificare i moduli
             this.cached = this.modules
         },
-        addComponent: function () {
+        addComponent: function() {
             this.$refs.componentSelector.show()
         },
-        newComponent: function (type) {
+        newComponent: function(type) {
             this.moduleType = type
             this.dismissModal()
 
@@ -154,29 +186,29 @@ export default {
             }
             this.cached.push(newModule)
         },
-        deleteComponent: function (component) {
+        deleteComponent: function(component) {
             let idx = this.cached.indexOf(component)
             if (idx > -1) {
                 this.cached.splice(idx, 1)
             }
         },
-        dismissModal: function () {
+        dismissModal: function() {
             this.$refs.componentSelector.hide()
         },
 
-        addComponentSide: function () {
+        addComponentSide: function() {
             this.$refs.sidebarSelector.show()
         },
-        deleteComponentSide: function (component) {
+        deleteComponentSide: function(component) {
             let idx = this.cachedSides.indexOf(component)
             if (idx > -1) {
                 this.cachedSides.splice(idx, 1)
             }
         },
-        dismissModalSide: function () {
+        dismissModalSide: function() {
             this.$refs.sidebarSelector.hide()
         },
-        newComponentSide: function (type) {
+        newComponentSide: function(type) {
             this.sideType = type
             this.dismissModal()
 
@@ -190,14 +222,14 @@ export default {
             }
             this.cachedSides.push(newSide)
         },
-        setModule: function (module) {
+        setModule: function(module) {
             console.log('deprecata');
         },
-        deleted: function (module) {
+        deleted: function(module) {
             this.moduleType = null
             this.$emit('deleted', module)
         },
-        deletedSide: function (sidebarModule) {
+        deletedSide: function(sidebarModule) {
             this.sideType = null
             this.$emit('deleted', sidebarModule)
         },
@@ -206,7 +238,7 @@ export default {
         //     this.sideType = null
         // }
     },
-    mounted: function () {
+    mounted: function() {
         this.init()
     },
 }
