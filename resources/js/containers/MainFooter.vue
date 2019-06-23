2044d621c38a1f8ce5545f6d6bb95e13fd27228e
Angus Simons <info@simonepozzobon.com>
2019-06-24 00:07:36 +0200

    Aggiunta testo nel footer

--- resources/js/containers/MainFooter.vue
+++ resources/js/containers/MainFooter.vue
@@ -36,6 +36,11 @@
                     </p>
                 </div>
             </div>
+            <div class="row">
+                <div class="col text-white py-5 text-center">
+                    Il Regolamento UE 2016/679 (di seguito GDPR) prevede e rafforza la protezione e il trattamento dei dati personali alla luce dei principi di correttezza, liceità, trasparenza, tutela della riservatezza e dei diritti dell’interessato in merito ai propri dati.  I dati personali saranno sottoposti a modalità di trattamento sia cartaceo sia elettronico e/o automatizzato, quindi con modalità sia manuali che informatiche. In ogni caso saranno adottate tutte le procedure idonee a proteggerne la riservatezza, nel rispetto delle norme vigenti e del segreto professionale.  Saranno utilizzate adeguate misure di sicurezza al fine di garantire la protezione, la sicurezza, l’integrità e l’accessibilità dei dati personali.  I dati personali saranno conservati solo per il tempo necessario al conseguimento delle finalità per le quali sono stati raccolti o per qualsiasi altra legittima finalità a essi collegata. I dati personali che non siano più necessari, o per i quali non vi sia più un presupposto giuridico per la relativa conservazione, verranno anonimizzati irreversibilmente o distrutti in modo sicuro.
+                </div>
+            </div>
         </div>
     </div>
 </template>
